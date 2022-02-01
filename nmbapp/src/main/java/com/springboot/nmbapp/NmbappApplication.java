918eb638054643bad71fa866e92c5e7e625c3dd3
munashemafovera24 <98808355+munashemafovera24@users.noreply.github.com>
2022-02-01 14:21:59 +0200

    Add files via upload

---
+++ nmbapp/src/main/java/com/springboot/nmbapp/NmbappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.nmbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmbappApplication.class, args);
+	}
+
+}
