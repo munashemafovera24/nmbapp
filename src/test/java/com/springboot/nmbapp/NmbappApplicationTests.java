dd5aff73e47a7a206639eaf147e2531850672850
munashemafovera24 <98808355+munashemafovera24@users.noreply.github.com>
2022-02-01 13:54:31 +0200

    Add files via upload

---
+++ src/test/java/com/springboot/nmbapp/NmbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.nmbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
